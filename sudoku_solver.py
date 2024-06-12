da358eedd4d626e693d35b103a74b017a31d4da8
Shreyas Jangam <shreyasjangamofficial@gmail.com>
2024-06-12 21:05:58 +0530

    Add files via upload

---
+++ sudoku_solver.py
@@ -0,0 +1,78 @@
+board = [
+    [0, 4, 9, 2, 0, 6, 0, 0, 7],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [7, 8, 0, 4, 0, 0, 1, 2, 0]
+]
+
+def solve(board):
+    empty_position = find_empty(board)
+    if not empty_position:
+        return True  # Solution found, board is complete
+    else:
+        row, col = empty_position
+
+    for num in range(1, 10):
+        if is_valid(board, num, (row, col)):
+            board[row][col] = num
+
+            if solve(board):
+                return True
+            
+            board[row][col] = 0  # Reset the cell for backtracking
+
+    return False
+
+def is_valid(board, num, pos):
+    # Check the row
+    for i in range(len(board[0])):
+        if board[pos[0]][i] == num and pos[1] != i:
+            return False
+
+    # Check the column
+    for i in range(len(board)):
+        if board[i][pos[1]] == num and pos[0] != i:
+            return False
+
+    # Check the 3x3 box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+
+    for i in range(box_y * 3, box_y * 3 + 3):
+        for j in range(box_x * 3, box_x * 3 + 3):
+            if board[i][j] == num and (i, j) != pos:
+                return False
+
+    return True
+
+def print_board(board):
+    for i in range(len(board)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty(board):
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+print_board(board)
+print(" ")
+solve(board)
+print(" ")
+print_board(board)
